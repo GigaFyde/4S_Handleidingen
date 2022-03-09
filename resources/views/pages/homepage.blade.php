c95ae7e04f059f53413a750349014cdc0413ba31
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-09 11:57:34 +0100

    Change homepage styling, closes #3

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -21,7 +21,7 @@
     $chunk_size = ceil($size / $columns);
     ?>
 
-    <div class="container">
+    <div class="container breadcrumb">
         <!-- Example row of columns -->
         <div class="row">
 
